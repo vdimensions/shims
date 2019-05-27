7ed73cac6202ba37ccb5c15ef77fdf29bb9adf07
ivaylo5ev <ivaylo5ev@gmail.com>
2019-05-27 15:29:31 +0300

    * added Func shim * added target framework net45

---
+++ src/System.Shim/Func..cs
@@ -0,0 +1,6 @@
+﻿namespace System
+{
+    #if NETFRAMEWORK && !NET40_OR_NEWER
+    public delegate TResult Func<T1, T2, T3, T4, T5, TResult>(T1 arg1, T2 args, T3 arg3, T4 arg4, T5 arg5);
+    #endif
+}
