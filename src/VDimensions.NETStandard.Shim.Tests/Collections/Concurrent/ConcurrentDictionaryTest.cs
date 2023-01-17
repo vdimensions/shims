e03556f4f932a95ef6c124233562c6f2b307bb07
Ivaylo Slavov <ivaylo5ev@gmail.com>
2023-01-17 23:55:41 +0200

    - updated submodules, fixing concurrent dictionary usages - added test coverage for concurrend dictionary assembly resolution

---
+++ src/VDimensions.NETStandard.Shim.Tests/Collections/Concurrent/ConcurrentDictionaryTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using NUnit.Framework;
+
+namespace VDimensions.NETStandard.Shim.Tests.Collections.Concurrent
+{
+    [TestFixture]
+    public class ConcurrentDictionaryTest
+    {
+        #if NETSTANDARD1_0
+        private const string ConcurrentDictionaryAssemblyName = "Portable.ConcurrentDictionary";
+        #elif NET35
+        private const string ConcurrentDictionaryAssemblyName = "System.Threading.Tasks.NET35";
+        #else
+        private const string ConcurrentDictionaryAssemblyName = "mscorlib";
+        #endif
+        
+        #if NET35_OR_NEWER
+        [Test]
+        public void TestAssemblyLevelGetCustomAttributeGenericCall()
+        {
+            var concurrentDictionary = new ConcurrentDictionary<string, int>();
+            var concurrentDictionaryAssembly = concurrentDictionary.GetType().Assembly;
+            Assert.AreEqual(ConcurrentDictionaryAssemblyName, concurrentDictionaryAssembly.GetName().Name);
+        }
+        #endif
+    }
+}
